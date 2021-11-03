07fd3e29d4cee2e25da46e9a621957e95f3a1627
WeryZebra-Yue <75676675+WeryZebra-Yue@users.noreply.github.com>
2021-11-03 22:31:05 +0530

    ss

--- servercode/index.js
+++ servercode/index.js
@@ -1,11 +1,18 @@
 const express = require('express')
 const app = express()
-
+app.use(
+    express.urlencoded({
+      extended: true
+    })
+  )
+  
+  app.use(express.json())
 app.post('/Hook', function(req, res){
     // console.log(res.);
-   req.on('data', function(data){
-       console.log(data);
-   });
+    req.on('end', () => {
+        console.log(JSON.parse(data)); // 'Buy the milk'
+        res.end();
+      })
 });
 
 app.listen(3000);
