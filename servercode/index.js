cec900221719e91096161bee63d1ea97edbc6318
WeryZebra-Yue <75676675+WeryZebra-Yue@users.noreply.github.com>
2021-11-04 08:50:10 +0530

    Setted up discord

--- servercode/index.js
+++ servercode/index.js
@@ -1,17 +1,48 @@
-const express = require('express')
+import express from 'express'
 const app = express()
 app.use(
     express.urlencoded({
       extended: true
     })
   )
-  
-  app.use(express.json())
-app.post('/Hook', function(req, res){
-    // console.log(res.);
-    console.log(req.body);
-});
+
+app.use(express.json())
+
 
 app.listen(3000);
 
 /// balh blah
+
+import dotenv from 'dotenv';
+import Discord, {  Intents } from 'discord.js';
+dotenv.config();
+const client = new Discord.Client({
+    intents:[
+       Intents.FLAGS.GUILDS,
+       Intents.FLAGS.GUILD_MESSAGES,
+    ]
+});
+const Channel = client.channels.cache.get('822334726890455053');
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+   
+    
+});
+let HelloInterval;
+client.on('messageCreate',(message)=>{
+    if(message.content.split(' ')[0] === 'git'){
+        
+      if(message.content.split(' ')[1].toLowerCase() === 'set'){
+          message.reply(`Setted Up to /${message.content.split(' ')[2]}`);
+          app.post(`/${message.content.split(' ')[2]}`, function(req, res){
+            // console.log(res.);
+            message.channel.send(`${req.body}`);
+        });
+        }
+       
+    }   
+    
+})
+
+client.login(process.env.TOKEN);
