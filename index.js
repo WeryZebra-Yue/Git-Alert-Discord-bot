7d4fe7371ce69640376f7d61cce278e55b49a656
WeryZebra-Yue <75676675+WeryZebra-Yue@users.noreply.github.com>
2021-11-04 13:34:43 +0530

    Solved Few bugs

--- index.js
+++ index.js
@@ -1,53 +1,50 @@
-const express = require('express');
+import express from 'express'
 const app = express()
 app.use(
   express.urlencoded({
       extended: true
     })
-  )
-  
-app.use(express.json())
-const port = process.env.PORT || 8000;
-
-app.set('port', port);
+    )
+    
+    app.use(express.json())
+    
+    
+app.listen(3000||process.env.PORT);
 app.get('/', (req, res) => {
   res.send('Hello World!');
 })
 /// balh blah
-const Discord = require('discord.js');
-const {Intents , Client} = require('discord.js');
-// const clients = new Client({ intents: [Intents.FLAGS.GUILDS] });
-const dotenv = require('dotenv');
+
+import dotenv from 'dotenv';
+import Discord, {  Intents } from 'discord.js';
 dotenv.config();
-const client = new Client({
+const client = new Discord.Client({
   intents:[
     Intents.FLAGS.GUILDS,
-    
+    Intents.FLAGS.GUILD_MESSAGES,
   ]
 });
-client.login(process.env.TOKEN);
-// const Channel = client.channels.cache.get('822334726890455053');
+const Channel = client.channels.cache.get('822334726890455053');
 
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-   
+  console.log(`Logged in as ${client.user.tag}!`);
+  
 });
-let route; 
+let route;
 
 client.on('messageCreate',(message)=>{
-    if(message.content.split(' ')[0] === 'git'){
-      console.log(message.content.split(' ')[1]);
-        
-      if(message.content.split(' ')[1].toLowerCase() === 'set'){
-          message.reply(`Setted Up to /${message.content.split(' ')[2]}`);
-          route = message.content.split(' ')[2]
-         webHook(route , message);
-        }
-       
-       
-    }  
-
-     
+  if(message.content.split(' ')[0] === 'git'){
+    
+    if(message.content.split(' ')[1].toLowerCase() === 'set'){
+      message.reply(`Setted Up to /${message.content.split(' ')[2]}`);
+      route = message.content.split(' ')[2]
+      webHook(route , message);
+    }
+    
+    
+  }  
+  
+  
 })
 function webHook(route , message){
   app.post(`/${route}`, function(req, res){
@@ -61,6 +58,7 @@ function webHook(route , message){
     console.log(req.body);
   });
 }
- // Just testingg
+// Just testingg
 
+client.login(process.env.TOKEN);
 
